35b80367b2100ec9c962f095748f60ae79416420
a1833554 <110015029+a1833554@users.noreply.github.com>
2022-08-23 16:18:47 +0930

    Add files via upload

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -19,36 +19,36 @@ void hexDigits(int *numbers,int length){
     for (int i = 0; i < length; i++){
 switch (numbers[i]){
 case 0:
-::cout<<i<<" "<<numbers[i]<<" 1"<<endl;
+::cout<<i<<" "<<numbers[i]<<" 0"<<endl;
 break;
 case 1:
-cout<<i<<" "<<numbers[i]<<" 2"<<endl;
+cout<<i<<" "<<numbers[i]<<" 1"<<endl;
 break;
 case 2:
-cout<<i<<" "<<numbers[i]<<" 3"<<endl;
+cout<<i<<" "<<numbers[i]<<" 2"<<endl;
 break;
 case 3:
-cout<<i<<" "<<numbers[i]<<" 4"<<endl;
+cout<<i<<" "<<numbers[i]<<" 3"<<endl;
 break;
 case 4:
-cout<<i<<" "<<numbers[i]<<" 5"<<endl;
+cout<<i<<" "<<numbers[i]<<" 4"<<endl;
 break;
 case 5:
-cout<<i<<" "<<numbers[i]<<" 6"<<endl;
+cout<<i<<" "<<numbers[i]<<" 5"<<endl;
 break;
 case 6:
-cout<<i<<" "<<numbers[i]<<" 7"<<endl;
+cout<<i<<" "<<numbers[i]<<" 6"<<endl;
 break;
 case 7:
-cout<<i<<" "<<numbers[i]<<" 8"<<endl;
+cout<<i<<" "<<numbers[i]<<" 7"<<endl;
 break;
 case 8:
-cout<<i<<" "<<numbers[i]<<" 9"<<endl;
+cout<<i<<" "<<numbers[i]<<" 8"<<endl;
 break;
 case 9:
-cout<<i<<" "<<numbers[i]<<" A"<<endl;
+cout<<i<<" "<<numbers[i]<<" 9"<<endl;
 case 10:
-cout<<i<<" "<<numbers[i]<<" B"<<endl;
+cout<<i<<" "<<numbers[i]<<" A"<<endl;
 break;
 case 11:
 cout<<i<<" "<<numbers[i]<<" C"<<endl;
@@ -63,7 +63,7 @@ case 14:
 cout<<i<<" "<<numbers[i]<<" F"<<endl;
 break;
 case 15:
-cout<<i<<" "<<numbers[i]<<" F"<<endl;
+cout<<i<<" "<<numbers[i]<<" G"<<endl;
 break;
 
 default:
